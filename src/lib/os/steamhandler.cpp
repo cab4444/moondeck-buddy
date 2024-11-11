2ddb55cb8c4f36fe17615808a5644af91ab8ae18
cab4444 <cab4444@gmail.com>
2024-11-10 22:16:19 -0500

    Update steamhandler.cpp

--- src/lib/os/steamhandler.cpp
+++ src/lib/os/steamhandler.cpp
@@ -134,9 +134,7 @@ bool SteamHandler::launchApp(uint app_id, bool force_big_picture)
             steam_process.setStandardOutputFile(QProcess::nullDevice());
             steam_process.setStandardErrorFile(QProcess::nullDevice());
             steam_process.setProgram(m_steam_exec_path);
-            steam_process.setArguments(QStringList() << "steam://open/bigpicture"
-                                                     << "-master_ipc_name_override"
-                                                     << "CobaltDuo");
+            steam_process.setArguments(QStringList() << "steam://open/bigpicture");
 
             if (!steam_process.startDetached())
             {
